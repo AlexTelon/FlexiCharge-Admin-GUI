93c51cc5838889a2d3869c92895ec1163c52721a
keriki101 <kireerik2008@hotmail.com>
2021-09-07 16:49:31 +0200

    Has started with the login components/design by adding login.css and login.tsx

--- react-app/src/components/login/Login.tsx
+++ react-app/src/components/login/Login.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Paper, makeStyles, createStyles, Theme } from '@material-ui/core';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Paper, makeStyles, createStyles, Theme, TextField, AppBar, Toolbar, Typography } from '@material-ui/core';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './Login.css';
+// import backgroundImage from '../../assets/background.svg';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     appBar: {
-      backgroundColor: '#ffffff'
+      backgroundColor: '#E5E5E5'
+    },
+    root: {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      alignContent: 'center',
+      justifyContent: 'center'
+    },
+    loginPaper: {
+      background: '#128541',
+      height: '5rem',
+      width: '15rem'
     }
   })
 );
@@ -14,7 +29,20 @@ const Login = () => {
   const classes = useStyles();
 
   return (
-    <Paper className={classes.appBar} elevation={3} />
+    <div className={classes.root}>
+      {/* <img src={backgroundImage} alt="BackgroundImage" /> */}
+      <Paper className={classes.loginPaper} elevation={10}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              Hello world
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <TextField label="username" variant="outlined" />
+        <TextField />
+      </Paper>
+    </div>
   );
 };
 
