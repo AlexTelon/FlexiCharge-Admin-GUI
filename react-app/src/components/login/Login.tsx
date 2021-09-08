aa9dd6a564650cc6ed88c1c0f565e68dbb7ea517
tifye <dematasjoshua@hotmail.com>
2021-09-08 12:57:00 +0200

    Fixed overall layout of login, along with logo and bootstrap, only specific styling left

--- react-app/src/components/login/Login.tsx
+++ react-app/src/components/login/Login.tsx
@@ -8,77 +8,85 @@ import backgroundTitle from '../../assets/title.png';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    appBar: {
-      backgroundColor: '#fffff'
-    },
     root: {
       position: 'absolute',
       width: '100%',
       height: '100%',
       display: 'flex',
-      alignContent: 'center',
+      flexDirection: 'column',
+      alignItems: 'center',
       justifyContent: 'center'
     },
     loginPaper: {
-      background: '#ffffff',
-      height: '40vh',
-      width: '25rem',
-      margin: '200px auto'
-    },
-    usernameField: {
-      marginTop: '50px',
-      marginLeft: '4.6rem',
-      margin: theme.spacing(1)
-    },
-    passwordField: {
-      marginTop: '15px',
-      marginLeft: '4.6rem'
+      background: '#ffffff'
     },
     buttonStyle: {
       marginTop: '25px',
-      marginLeft: '9.5rem'
+      borderRadius: '1.5rem',
+      width: '2rem',
+      height: '1.5rem',
+      backgroundColor: '#78bd76',
+      color: '#ffffff',
+      marginBottom: '.5rem',
+      border: 'none'
     },
-    LogoTitle: {
-      display: 'flex',
-      justifyContent: 'center',
-      margin: '20px auto'
+    appBar: {
+      backgroundColor: '#78bd76',
+      marginBottom: '1rem'
+    },
+    inputField: {
+      marginTop: '1rem'
     }
   })
 );
+
+const LoginFields = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <div className="login-input-fields">
+        <TextField className={classes.inputField}
+          id="input-with-icon-textfield" label="username" variant="filled" InputProps={{
+            startAdornment: (<InputAdornment position="start">
+              <Person />
+            </InputAdornment>)
+          }} />
+        <TextField className={classes.inputField}
+          id="input-with-icon-textfield" label="password" variant="filled" InputProps={{
+            startAdornment: (<InputAdornment position="start">
+              <Lock />
+            </InputAdornment>)
+          }} />
+        <Button className={classes.buttonStyle} variant="outlined">Login</Button>
+      </div>
+    </>
+  );
+};
 
 const Login = () => {
   const classes = useStyles();
 
   return (
-    <><div>
-      <img className={classes.LogoTitle} src={backgroundLogo} alt="1234" />
-      <img className={classes.LogoTitle} src={backgroundTitle} alt="1234" />
-    </div><div className={classes.root}>
-      <Paper className={classes.loginPaper} elevation={7}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6">
-              Admin Portal
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <div>
-          <TextField className={classes.usernameField}
-            id="input-with-icon-textfield" label="username" variant="outlined" InputProps={{
-              startAdornment: (<InputAdornment position="start">
-                <Person />
-              </InputAdornment>)
-            }} />
-          <TextField className={classes.passwordField}
-            id="input-with-icon-textfield" label="password" variant="outlined" InputProps={{
-              startAdornment: (<InputAdornment position="start">
-                <Lock />
-              </InputAdornment>)
-            }} />
-          <Button className={classes.buttonStyle} variant="outlined">Login</Button>
+    <>
+      <div className="login-page">
+        <div className="flexicharge-logo-title">
+          <img src={backgroundLogo} alt="1234" />
+          <img src={backgroundTitle} alt="1234" />
         </div>
-      </Paper>
-    </div></>
+        <div className="login-form-wrapper col-10 col-sm-5">
+          <Paper className={classes.loginPaper} elevation={7}>
+            <AppBar className={classes.appBar} position="static">
+              <Toolbar>
+                <Typography variant="h6">
+                  Admin Portal
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <LoginFields />
+          </Paper>
+        </div>
+      </div>
+    </>
   );
 };
 
