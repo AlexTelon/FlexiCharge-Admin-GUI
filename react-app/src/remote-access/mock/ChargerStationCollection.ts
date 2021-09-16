3b4433a0971b98b7881a19ff07262b60d7c043a8
tifufu <dematasjoshua@hotmail.com>
2021-09-16 07:21:31 +0200

    error handling and success of add charger station

--- react-app/src/remote-access/mock/ChargerStationCollection.ts
+++ react-app/src/remote-access/mock/ChargerStationCollection.ts
@@ -22,4 +22,21 @@ export default class ChargerStationCollection implements IChargerStationCollecti
       }, 100);
     });
   }
+
+  async addChargerStation(fields: Omit<ChargerStation, 'id'>): Promise<[string | null, any | null]> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        for (const station of this.stations) {
+          if (station.name === fields.name) resolve([null, { error: true, errorMessage: 'Charger Station name already taken' }]);
+        }
+
+        const chargerStation: ChargerStation = {
+          ...fields,
+          id: `${this.stations.length + 1}`
+        };
+        this.stations.push(chargerStation);
+        resolve([chargerStation.id, null]);
+      }, 1000);
+    });
+  }
 }
