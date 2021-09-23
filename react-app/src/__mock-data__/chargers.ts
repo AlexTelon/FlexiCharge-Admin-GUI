18c1f962b17b24336a7d22dd9e1ed50f2ee5865d
keriki101 <kireerik2008@hotmail.com>
2021-09-23 16:51:53 +0200

    Added realistic Mock data and fixed minor bugs

--- react-app/src/__mock-data__/chargers.ts
+++ react-app/src/__mock-data__/chargers.ts
@@ -1,13 +1,7 @@
 import { ChargerStation } from '../remote-access/interfaces';
 
 export const chargerStations: ChargerStation[] = [
-  { id: '1', name: 'Station One', address: 'Airport', longitude: 1, latitude: 1 },
-  { id: '2', name: 'Station Two', address: 'University', longitude: 2, latitude: 2 },
-  { id: '3', name: 'Station Three', address: 'Skynet HQ', longitude: 3, latitude: 3 },
-  { id: '4', name: 'Station One', address: 'Airport', longitude: 1, latitude: 1 },
-  { id: '5', name: 'Station Two', address: 'University', longitude: 2, latitude: 2 },
-  { id: '6', name: 'Station Three', address: 'Skynet HQ', longitude: 3, latitude: 3 },
-  { id: '7', name: 'Station One', address: 'Airport', longitude: 1, latitude: 1 },
-  { id: '8', name: 'Station Two', address: 'University', longitude: 2, latitude: 2 },
-  { id: '9', name: 'Station Three', address: 'Skynet HQ', longitude: 3, latitude: 3 }
+  { id: '1', name: 'University Station', address: 'Gjuterigatan 3', longitude: 57.7788187, latitude: 14.1621992 },
+  { id: '2', name: 'McDonalds Station', address: 'Hagaleden 3', longitude: 57.7624059, latitude: 14.1499653 },
+  { id: '3', name: 'Asect Station', address: 'Kompanigatan 1', longitude: 57.7718868, latitude: 14.2020042 }
 ];
--- react-app/src/__mock-data__/users.ts
+++ react-app/src/__mock-data__/users.ts
@@ -1,8 +1,8 @@
 import { ManageUser } from '../remote-access/interfaces';
 
 export const manageUsers: ManageUser[] = [
-  { id: '1', name: 'Jon Doe', payment: 'Swish', role: 'Admin' },
-  { id: '2', name: 'Johan Svensson', payment: 'Invoices', role: 'Admin' },
+  { id: '1', name: 'Jon Doe', payment: 'Klarna', role: 'User' },
+  { id: '2', name: 'Johan Svensson', payment: 'Invoices', role: 'User' },
   { id: '3', name: 'Erik Granlund', payment: 'Klarna', role: 'Admin' },
-  { id: '4', name: 'Du Won Htoo', payment: 'Swish', role: 'Admin' }
+  { id: '4', name: 'Du Won Htoo', payment: 'Invoices', role: 'Admin' }
 ];
