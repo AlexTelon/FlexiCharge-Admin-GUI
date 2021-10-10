baef464aef3aa2f47e48c5727c1546e994be8fc4
tifufu <dematasjoshua@hotmail.com>
2021-10-10 20:10:21 +0200

    Created new AdminCollection file

---
+++ react-app/src/remote-access/remote/AdminCollection.ts
@@ -0,0 +1,76 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { manageAdmins } from '../../__mock-data__/admins';
+import { ManageAdmin, IManageAdminCollection } from '../types';
+
+export default class ManageAdminCollection implements IManageAdminCollection {  
+  admins = manageAdmins;
+
+  async getAllAdmins(): Promise<ManageAdmin[]> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.admins);
+      }, 1000);
+    });
+  }
+
+  async getAdminById(adminId: string): Promise<ManageAdmin | null> {
+    return new Promise((resolve, reject) => {
+      // Look up in local
+      // If not found then try remote
+
+      setTimeout(() => {
+        resolve(this.admins.filter((admins) => admins.id === adminId)[0] || null);
+      }, 100);
+    });
+  }
+
+  async addAdmin(fields: Omit<ManageAdmin, 'id'>): Promise<[string | null, any | null]> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const errorObj = this.validateFields(fields);
+        if (Object.keys(errorObj).length > 0) resolve([null, errorObj]);
+        
+        const manageAdmin: ManageAdmin = {
+          ...fields,
+          username: `${this.admins.length + 1}`
+        };
+        this.admins.push(manageAdmin);
+        resolve([manageAdmin.username, null]);
+      }, 1000);
+    });
+  }
+
+  async updateAdmin(adminId: string, fields: Omit<ManageAdmin, 'id'>): Promise<[ManageAdmin | null, any | null]> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const adminIndex = this.admins.findIndex((admins) => admins.id === adminId);
+        if (adminIndex === -1) return [null, { errorMessage: 'Could not find the requested Manage Admin' }];
+
+        const errorObj = this.validateFields(fields);
+        if (Object.keys(errorObj).length > 0) resolve([null, errorObj]);
+
+        const ManageAdmins = {
+          ...fields,
+          id: this.admins[adminIndex].id
+        };
+
+        this.admins[adminIndex] = ManageAdmins;
+        resolve([ManageAdmins, null]);
+      }, 1000);
+    });
+  }
+
+  private isNametaken(name: string) {
+    for (const admin of this.admins) {
+      if (admin.name === name) return true;
+    }
+    return false;
+  }
+
+  private validateFields(fields: Omit<ManageAdmin, 'id'>): any | null {
+    const errorObj: any = {};
+    if (fields.name && this.isNametaken(fields.name)) errorObj.name = 'Name is taken';
+    return errorObj;
+  }
+}
