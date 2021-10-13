9670f121a4b8238e18df0e6a79e0a8a3fd8184ca
tifufu <dematasjoshua@hotmail.com>
2021-10-13 15:42:19 +0200

    Raised base klarnaReservationAmount

--- react-app/src/components/pages/charger-stations/AddStationDialog.tsx
+++ react-app/src/components/pages/charger-stations/AddStationDialog.tsx
@@ -49,7 +49,7 @@ const AddSingleStationDialog = ({ open, handleClose }: any) => {
         name: fields.name,
         location: [fields.longitude, fields.latitude],
         price: fields.price,
-        klarnaReservationAmount: 5000
+        klarnaReservationAmount: 50000
       });
       
       if (result[1] !== null) {
