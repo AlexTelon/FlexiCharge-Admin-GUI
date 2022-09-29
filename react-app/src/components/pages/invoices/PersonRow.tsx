3b6cf5adec9bdea099fd2c5920e1eb35f88d48da
kyrollosceriacous <kyrollosceriacous@gmail.com>
2022-09-29 12:28:25 +0200

    Added dummy pdf that is triggered when clicking link

--- react-app/src/components/pages/invoices/PersonRow.tsx
+++ react-app/src/components/pages/invoices/PersonRow.tsx
@@ -2,9 +2,11 @@
 /* eslint-disable react/jsx-no-undef */
 import { ManageUser } from '@/remote-access/types';
 import {
-  Theme, useTheme, TableRow, TableCell, Box, Typography, Checkbox
+  Theme, useTheme, TableRow, TableCell, Box, Typography,
 } from '@material-ui/core';
 import { FC } from 'react';
+// ignore below triggered error regarding imported invoice PDF
+import pdf from './dummy invoice.pdf';
 
 interface PersonRowProps {
   person: ManageUser
@@ -46,7 +48,7 @@ const PersonRow: FC<PersonRowProps> = ({ person, classes }) => {
             {person.name}
           </Typography>
         </TableCell>
-        <TableCell style={{ textDecoration: 'underline' }}>invoice-september.pdf</TableCell>
+        <TableCell><a href={pdf} target="_blank" rel="noreferrer">View invoice</a></TableCell>
       </TableRow>
     </>
   );
