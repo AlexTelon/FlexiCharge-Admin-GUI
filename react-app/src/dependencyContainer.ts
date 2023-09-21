6953868a3ce2ba774cc2c044edd5a23c50eea6be
WinterBoii <wintercodess@gmail.com>
2023-09-21 13:56:09 +0200

    feat: adding test

--- react-app/src/dependencyContainer.ts
+++ react-app/src/dependencyContainer.ts
@@ -3,23 +3,22 @@
 import { createContainer, asClass, Lifetime, InjectionMode } from 'awilix';
 // change /mock/ to /remote/ to use api data
 
-import AuthenticationProvider from './remote-access/remote/AuthenticationProvider';
+/* import AuthenticationProvider from './remote-access/remote/AuthenticationProvider';
 import ManageUser from './remote-access/remote/ManageUser';
 import ManageAdmin from './remote-access/remote/ManageAdmin';
 import ManageTransaction from './remote-access/remote/ManageTransaction';
 import ManageInvoice from './remote-access/remote/ManageInvoice';
 import ManageChargerStation from './remote-access/remote/ManageChargerStation';
-import ManageCharger from './remote-access/remote/ManageCharger';
+import ManageCharger from './remote-access/remote/ManageCharger'; */
 
-/*
 import AuthenticationProvider from './remote-access/mock/AuthenticationProvider';
 import ManageUser from './remote-access/mock/ManageUser';
 import ManageAdmin from './remote-access/mock/ManageAdmin';
 import ManageTransaction from './remote-access/mock/ManageTransaction';
 import ManageInvoice from './remote-access/mock/ManageInvoice';
-import ManageChargerStation from './remote-access/remote/ManageChargerStation';
-import ManageCharger from './remote-access/remote/ManageCharger';
-*/
+import ManageChargerStation from './remote-access/mock/ManageChargerStation';
+import ManageCharger from './remote-access/mock/ManageCharger';
+
 
 const container = createContainer({
   injectionMode: InjectionMode.CLASSIC
