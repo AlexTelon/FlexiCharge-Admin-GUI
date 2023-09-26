c9112d556b7e6d37f8d658399a4da59d60de79a4
Viktor Johansson <vikjoh11@gmail.com>
2023-09-27 00:35:34 +0200

    Fix dependencyContainer to not be in conflict with dev

--- react-app/src/dependencyContainer.ts
+++ react-app/src/dependencyContainer.ts
@@ -2,15 +2,15 @@
 /* eslint-disable react/jsx-no-undef */
 import { createContainer, asClass, Lifetime, InjectionMode } from 'awilix';
 // change /mock/ to /remote/ to use api data
-
-/* import AuthenticationProvider from './remote-access/remote/AuthenticationProvider';
+/* 
+import AuthenticationProvider from './remote-access/remote/AuthenticationProvider';
 import ManageUser from './remote-access/remote/ManageUser';
 import ManageAdmin from './remote-access/remote/ManageAdmin';
 import ManageTransaction from './remote-access/remote/ManageTransaction';
 import ManageInvoice from './remote-access/mock/ManageInvoice';
 import ManageChargerStation from './remote-access/remote/ManageChargerStation';
-import ManageCharger from './remote-access/remote/ManageCharger'; */
-
+import ManageCharger from './remote-access/remote/ManageCharger'; 
+*/
 
 import AuthenticationProvider from './remote-access/mock/AuthenticationProvider';
 import ManageUser from './remote-access/mock/ManageUser';
@@ -18,7 +18,7 @@ import ManageAdmin from './remote-access/mock/ManageAdmin';
 import ManageTransaction from './remote-access/mock/ManageTransaction';
 import ManageInvoice from './remote-access/mock/ManageInvoice';
 import ManageChargerStation from './remote-access/remote/ManageChargerStation';
-import ManageCharger from './remote-access/mock/ManageCharger';
+import ManageCharger from './remote-access/remote/ManageCharger';
 
 
 const container = createContainer({
