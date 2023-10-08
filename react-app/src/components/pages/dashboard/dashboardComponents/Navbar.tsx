dff9c8acf139fc34b290454493371743c3a51784
Ville-Wilson <vilhelm.lindahl@telia.com>
2023-10-08 15:57:07 +0200

    Remove unused imports

--- react-app/src/components/pages/dashboard/dashboardComponents/Navbar.tsx
+++ react-app/src/components/pages/dashboard/dashboardComponents/Navbar.tsx
@@ -4,8 +4,6 @@ import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/sty
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
-// import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-// import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
